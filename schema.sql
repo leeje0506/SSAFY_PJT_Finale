ded371eeec1e8b6c43231c687c90d6e3b735eabe
leeje0506 <137889331+leeje0506@users.noreply.github.com>
2023-11-16 21:47:08 +0900

    upload new_schema

--- schema.sql
+++ schema.sql
@@ -1,40 +1,59 @@
 use final_ssafit;
 
-
-create table User (
-User_Id VARCHAR(100),
-User_Password VARCHAR(100),
-User_Name VARCHAR(100),
-User_NickName VARCHAR(100),
-User_Authority int(1),
-PRIMARY KEY(User_Id)
+create table calendar(
+    id int primary key,
+    groupId int,
+    title varchar(50),
+    writer varchar(50),
+    content varchar(1000),
+    start1 date,
+    end1 date,
+    allDay int(1),
+    textColor varchar(50),
+    backgroundColor varchar(50),
+    borderColor varchar(50)
 );
 
 
 
+Alter table user add User_Cha INT;
 
-create table board (
-Board_Type INT, 
-Board_Id INT AUTO_INCREMENT, 
-Board_Title VARCHAR(100),
-User_Id VARCHAR(100),
-Board_Contents VARCHAR(16000),
-Board_Date DATE,
-Board_ViewCnt INT,
-Board_Like INT,
-PRIMARY KEY(Board_Id),
-FOREIGN KEY (User_Id) REFERENCES user(User_Id) ON UPDATE CASCADE
+
+
+create table restaurant(
+    Res_Id int primary key,
+	Res_Name varchar(100),
+    Res_Region varchar(100),
+    Res_Contury varchar(100),
+    Res_Lati float,
+    Res_Longi float,
+    Res_Address varchar(100),
+    Res_Tel varchar(100),
+    Res_WDay varchar(100),
+    Res_WEnd varchar(100),
+    Res_Park varchar(100),
+    Res_Kid varchar(100),
+    Res_Wheel varchar(100),
+    Res_Pet varchar(100),
+	Res_Vegi varchar(100),
+    Res_Halal varchar(100),
+    Res_GFree varchar(100)
 );
 
-create table chatting (
-User_Id VARCHAR(100),
-Board_Id INT,
-Chatting_Log VARCHAR(16000),
-Chatting_Time TIMESTAMP,
-FOREIGN KEY (Board_Id) REFERENCES board(Board_Id) ON UPDATE CASCADE,
-FOREIGN KEY (User_Id) REFERENCES user(User_Id) ON UPDATE NO ACTION
+
+
+create table review(
+	User_Id varchar(100),
+    Res_Id int,
+    Review_Content varchar(8000),
+    Review_Thumbnail varchar(8000),
+	
+    FOREIGN KEY (User_Id) REFERENCES user(User_Id) ,
+    FOREIGN KEY (Res_Id) REFERENCES restaurant(Res_Id) 
 );
 
+Alter table review MODIFY Review_Content varchar(3000);
+Alter table review MODIFY Review_Thumbnail varchar(13000);
 
 
 
